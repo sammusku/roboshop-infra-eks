e08f84e6b709eedbc2ef9a07618fd119d5665ee9
sammusku <Samyuktha.musku@gmail.com>
2026-04-22 13:29:42 -0500

    infra-eks

---
+++ 30-bastion/locals.tf
@@ -0,0 +1,15 @@
+locals {
+    ami_id = data.aws_ami.devops.id
+    
+     common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
+
+ # public subnet in 1a AZ
+ #converting string to list
+public_subnet_id = split (",", data.aws_ssm_parameter.public_subnet_ids.value )[0]
+bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+}
+ 
