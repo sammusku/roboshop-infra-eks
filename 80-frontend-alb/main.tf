84b5aef06f699483563379a05f409329633fd671
sammusku <Samyuktha.musku@gmail.com>
2026-04-23 13:35:15 -0500

    eks

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "frontend_alb" {
   name               = "${var.project}-${var.environment}-frontend-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [local.frontend_alb_sg_id]
+  security_groups    = [local.ingress_alb_sg_id]
   subnets            = local.public_subnet_ids
 
   enable_deletion_protection = false
@@ -34,6 +34,45 @@ resource "aws_lb_listener" "https" {
     }
   }
 }
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "${var.project}-${var.environment}-frontend"
+  port     = 80
+  protocol = "HTTP"
+  # if this is VM target group, then target_type should be instance. if target_type is IP pods will come and register here
+  target_type = "ip"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60
+
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path = "/"
+    port = 80
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 3
+  }
+}
+
+# This depends on target group
+resource "aws_lb_listener_rule" "frontend" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.project}-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
+
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
